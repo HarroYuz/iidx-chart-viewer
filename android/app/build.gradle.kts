0c04f3f023b4de2ae2815512c9c0c293eab13ffc
HarroYuz <hjhkgame@163.com>
2026-09-15 20:39:25 +0800

    Keep current updates on the 1.1.x version line as 1.1.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.harroyuz.iidxchartviewer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.2.1"
+        versionCode = 20
+        versionName = "1.1.5"
     }
 
     signingConfigs {
